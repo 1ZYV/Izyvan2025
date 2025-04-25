3c1ed151f7098f95f00a30ccd89c8be1383ebc12
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-04-25 03:51:02 -0500

    feat: define application routes for authentication and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './modules/auth/components/sign-in/sign-in.component';
+import { AuthLayoutComponent } from './modules/auth/layouts/auth-layout/auth-layout.component';
+import { SignUpComponent } from './modules/auth/components/sign-up/sign-up.component';
+import { DashboardLayoutComponent } from './modules/core/layouts/dashboard-layout/dashboard-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        children: [
+          {
+            path: 'sign-in',
+            component: SignInComponent,
+          },
+          {
+            path: 'sign-up',
+            component: SignUpComponent,
+          },
+        ],
+      },
+
+      {
+        path: 'dashboard',
+        component: DashboardLayoutComponent,
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'auth/sign-in',
+  },
+];
