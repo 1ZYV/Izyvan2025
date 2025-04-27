967eab08812d3f76d5f6a6e47738a3312a6312ec
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-04-27 00:25:39 -0500

    feat: add list-service-request and service-request components with basic structure and tests
    - Created ListServiceRequestComponent with HTML, CSS, and spec files.
    - Created ServiceRequestComponent with HTML, CSS, and spec files.
    - Added basic test cases to ensure components are created successfully.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SignInComponent } from './modules/auth/components/sign-in/sign-in.compo
 import { AuthLayoutComponent } from './modules/auth/layouts/auth-layout/auth-layout.component';
 import { SignUpComponent } from './modules/auth/components/sign-up/sign-up.component';
 import { DashboardLayoutComponent } from './modules/core/layouts/dashboard-layout/dashboard-layout.component';
+import { ListServiceRequestComponent } from './modules/service-request/components/list-service-request/list-service-request.component';
 
 export const routes: Routes = [
   {
@@ -24,6 +25,30 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardLayoutComponent,
+    children: [
+      {
+        path: 'services',
+        component: ListServiceRequestComponent,
+      },
+      {
+        path: 'history',
+      },
+      {
+        path: 'vehicles',
+      },
+      {
+        path: 'guides',
+      },
+      {
+        path: 'tariffs',
+      },
+      {
+        path: 'billing',
+      },
+      {
+        path: 'reports',
+      },
+    ],
   },
 
   {
---
+++ src/app/modules/service-request/components/list-service-request/list-service-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-service-request',
+  imports: [],
+  templateUrl: './list-service-request.component.html',
+  styleUrl: './list-service-request.component.css'
+})
+export class ListServiceRequestComponent {
+
+}
