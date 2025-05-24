4d4581bf788b2be070e033b720ac37514214bd87
Danilo <danilo.arenasyi@gmail.com>
2025-05-24 10:35:39 -0500

    feat: enhance service request form with additional fields for date, time, reference, passenger number, and description; implement reactive form handling

--- src/app/modules/service-request/components/service-request/service-request.component.ts
+++ src/app/modules/service-request/components/service-request/service-request.component.ts
@@ -3,6 +3,8 @@ import { ServiceRequestMapDirectionComponent } from '../../utils/service-request
 import { GoogleMapsModule } from '@angular/google-maps'; // Module for Google Maps integration
 import { PlaceAutocompleteComponent } from '../../../core/lib/components/place-autocomplete/place-autocomplete.component'; // Component for place autocomplete input
 import { VehicleTypeSelectorComponent } from '../../../core/lib/components/vehicle-type-selector/vehicle-type-selector.component'; // Component for selecting vehicle type
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BookingServiceService } from '../../services/booking-service.service';
 
 @Component({
   selector: 'app-service-request', // CSS selector for using this component
@@ -12,6 +14,7 @@ import { VehicleTypeSelectorComponent } from '../../../core/lib/components/vehic
     GoogleMapsModule,
     PlaceAutocompleteComponent,
     VehicleTypeSelectorComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './service-request.component.html', // Path to the HTML template
   styleUrl: './service-request.component.css', // Path to the CSS styles
@@ -22,7 +25,37 @@ export class ServiceRequestComponent {
   // Signal for the map's zoom level, default to 5
   zoom = signal<number>(5);
 
-  handleSumbitServiceRequest(){
-    
+  constructor(private bookingService: BookingServiceService) {}
+
+  serviceRequestForm : FormGroup = new FormGroup<{
+    includeTourismGuide: FormControl<boolean>,
+    nameReference: FormControl<string>,
+    passengerNumber: FormControl<number>,
+    description: FormControl<string>,
+    date: FormControl<string>,
+    time: FormControl<string>,
+  }>({
+    includeTourismGuide: new FormControl<boolean>(false, { nonNullable: true }),
+    nameReference: new FormControl<string>("", { nonNullable: true }),
+    passengerNumber: new FormControl<number>(0, { nonNullable: true }),
+    description: new FormControl<string>("", { nonNullable: true }),
+    date: new FormControl<string>("", { nonNullable: true }),
+    time: new FormControl<string>("", { nonNullable: true }),
+  });
+
+  handleSubmitServiceRequest(){
+    if (this.serviceRequestForm.valid) {
+      const formData = this.serviceRequestForm.value;
+      
+      this.bookingService.date.set(formData.date || "");
+      this.bookingService.time.set(formData.time || "");
+      this.bookingService.includeTourismGuide.set(formData.includeTourismGuide || false);
+      this.bookingService.nameReference.set(formData.nameReference || "");
+      this.bookingService.passengerNumber.set(formData.passengerNumber || 0);
+      this.bookingService.description.set(formData.description || "");
+      this.bookingService.bookingServiceRequest(); // Call the service to book the request
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
--- src/app/modules/service-request/services/booking-service.service.ts
+++ src/app/modules/service-request/services/booking-service.service.ts
@@ -43,11 +43,11 @@ export class BookingServiceService {
     this.http.post('http://localhost:8080/api/v1/service-request', request).subscribe({
       next: (response) => {
         console.log('Service request booked successfully:', response);
-        this.router.navigate(['/tariffs/create']);
+        this.router.navigate(['/dashboard/tariffs/create']);
       },
       error: (error) => {
         console.error('Error booking service request:', error);
-        this.router.navigate(['/tariffs/create']);
+        this.router.navigate(['/dashboard/tariffs/create']);
       }
     });
   }
